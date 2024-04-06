14f24fe7e56e05b0a51e4182f85e05d60bc8a5ff
Timothy Karani <kartimothy@gmail.com>
2024-04-06 15:04:41 +0300

    Added encoder/decoder service and beans

---
+++ src/main/java/tech/c3n7/muziki/security/service/JwtTokenService.java
@@ -0,0 +1,41 @@
+package tech.c3n7.muziki.security.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.stream.Collectors;
+
+@Service
+public class JwtTokenService {
+    public final JwtEncoder jwtEncoder;
+
+    public JwtTokenService(JwtEncoder jwtEncoder) {
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+        String scope = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .filter(authority -> !authority.startsWith("ROLE"))
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(authentication.getName())
+                .claim("scope", scope)
+                .build();
+
+        var encoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS512).build(), claims);
+        return this.jwtEncoder.encode(encoderParameters).getTokenValue();
+    }
+}
