77e22e226bd92edf8f09439147cf30e8538063df
Timothy Karani <kartimothy@gmail.com>
2024-05-26 19:32:04 +0300

    CHeckpoint

--- src/main/java/tech/c3n7/muziki/core/advice/ApplicationExceptionHandler.java
+++ src/main/java/tech/c3n7/muziki/core/advice/ApplicationExceptionHandler.java
@@ -1,6 +1,8 @@
 package tech.c3n7.muziki.core.advice;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,12 +15,30 @@ import java.util.Map;
 public class ApplicationExceptionHandler {
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleArgumentNotValid(MethodArgumentNotValidException exception) {
+    public Map<String, String> handle(MethodArgumentNotValidException exception) {
         Map<String, String> errorMap = new HashMap<>();
         exception.getBindingResult().getFieldErrors()
                 .forEach(error ->
                     errorMap.put(error.getField(), error.getDefaultMessage())
                 );
+
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public Map<String, String> handle(UsernameNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("message", exception.getMessage());
+
+        return errorMap;
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler(BadCredentialsException.class)
+    public Map<String, String> handle(BadCredentialsException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("message", exception.getMessage());
 
         return errorMap;
     }
